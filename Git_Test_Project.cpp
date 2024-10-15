88f0a4e6408e2afd01743670bbf6098689580170
keno555552 <kwankinleung5@gmail.com>
2024-10-15 10:49:14 +0900

    4444

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -9,7 +9,7 @@ int main()
 
     std::cout << "Hello New branch!\n";
 
-    std::cout << "PullRequest branch changed 66\n";
+    std::cout << "PullRequest branch changed 23\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
