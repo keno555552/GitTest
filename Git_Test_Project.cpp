abb5dab70e85b66639ec4ce9fd7fd7b9ec33fd34
keno555552 <kwankinleung5@gmail.com>
2024-10-15 10:58:54 +0900

    88

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -10,6 +10,12 @@ int main()
     std::cout << "Hello New branch!\n";
 
     std::cout << "PullRequest branch changed 23333\n";
+
+    <<<< < HEAD
+        printf("masterをローカルで変更!");
+    ======
+        printf("Pull");
+
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
